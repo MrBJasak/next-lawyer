2e7040472c659f8720d42c56ca44c7b3ba5d871c
MrBJasak <mrbjasak@gmail.com>
2025-02-02 01:27:57 +0100

    feat:Change default pin on map

--- src/containers/Contact/Contact.tsx
+++ src/containers/Contact/Contact.tsx
@@ -160,8 +160,8 @@ export const Contact = () => {
               }}
             >
               <iframe
-                src='https://www.google.com/maps/embed?pb=YOUR_MAPS_EMBED_URL'
-                width='100%'
+                src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2507.526634136133!2d19.422144!3d51.061827799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4fa2a3ca1477d6e3%3A0xd166f7842b9e0ca!2sAdwokat%20Agnieszka%20Jasak%20Kancelaria%20Adwokacka!5e0!3m2!1spl!2spl!4v1738455796289!5m2!1spl!2spl'
+                width='600'
                 height='450'
                 style={{ border: 0 }}
                 allowFullScreen
